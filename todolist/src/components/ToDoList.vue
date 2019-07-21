c18f6546f97da8625142cdf01f9087c2317bc24d
SamsonLin <153011490@qq.com>
2019-07-21 12:40:16 +0800

    feat:add  to do item

--- todolist/src/components/ToDoList.vue
+++ todolist/src/components/ToDoList.vue
@@ -7,12 +7,12 @@
             </p>
         </div>
         <div>
-            <input class="input-text" type="text" name="ListItem" />
-            <div id="button">Add</div>
+            <input class="input-text" type="text" name="ListItem" v-model="item"/>
+            <div id="button" @click="addItem">Add</div>
         </div>
         <br/>
         <ol>
-            <li>item</li>
+            <li v-for="item in itemList" :key="item.id" :id="uuid()"><input type="checkbox">{{item.content}}</li>
         </ol>
         <div>
             <ul id="filters">
@@ -33,7 +33,33 @@
 
 <script>
 export default {
-    name:"ToDoList"
+    name:"ToDoList",
+    data(){
+        return{
+            item:'',
+            itemList:[]
+        }
+    },
+    methods:{
+        addItem(){
+            if(this.item!=''){
+                this.itemList.push({id:1,content:this.item});
+                this.item='';
+            }
+        },
+        uuid(){
+            var s = [];
+            var hexDigits = "0123456789abcdef";
+           for (var i = 0; i < 36; i++) {
+            s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+            }
+            s[14] = "4"; 
+            s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); 
+            s[8] = s[13] = s[18] = s[23] = "-";
+            var uuid = s.join("");
+            return uuid;
+        }
+    }
 }
 </script>
 
